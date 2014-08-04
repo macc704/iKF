bf5f6c28ef3eed81699c872ef8af3d533c529396
Yoshiaki Matsuzawa <yoshiaki.matsuzawa@gmail.com>
2014-08-04 18:14:40 -0400

    Local tinyMCE part I

---
+++ iKF/WebResources/kfmobile.js
@@ -0,0 +1,34 @@
+/**
+ * kfmobile.js Mobile support Javascript Knowledge Forum
+ * 
+ * Original Author: Yoshiaki Matsuzawa 22 June,2014
+ */
+
+function replacePostReferenceTag() {
+	var elements = document.getElementsByTagName("kf-post-reference");
+	for (var i = 0; i < elements.length; i++) {
+		var element = elements[i];
+		var postId = element.getAttribute("postId");
+		var original = element.innerHTML;
+		elements[i].innerHTML = "<a href='/kforum/rest/mobile/getNoteAsHTMLwJS/"
+				+ postId + "'>" + "<img src='https://dl.dropboxusercontent.com/u/11409191/ikf/note.png' width=15px, height=15px/>"+ original + "</a>";
+	}
+}
+
+function replaceContentReferenceTag() {
+	var elements = document.getElementsByTagName("kf-content-reference");
+	for (var i = 0; i < elements.length; i++) {
+		var element = elements[i];
+		var postId = element.getAttribute("postId");
+		var original = element.innerHTML;
+		elements[i].innerHTML = "<span style='font-style:italic;'>\""+original+"\"</span>" + "(<a href='/kforum/rest/mobile/getNoteAsHTMLwJS/"
+				+ postId + "'>" + "<img src='https://dl.dropboxusercontent.com/u/11409191/ikf/note.png' width=15px, height=15px/>" + "</a>)";
+	}
+}
+
+function onLoadHook() {
+	replacePostReferenceTag();
+	replaceContentReferenceTag();
+}
+
+window.onload = onLoadHook;
