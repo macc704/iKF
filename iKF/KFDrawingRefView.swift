9fc69e2f59301026274ee63bfa253a08673dd878
Yoshiaki Matsuzawa <yoshiaki.matsuzawa@gmail.com>
2014-09-11 12:57:51 -0400

    Updated temporary solution for ios7 image problem

--- iKF/KFDrawingRefView.swift
+++ iKF/KFDrawingRefView.swift
@@ -12,6 +12,7 @@ class KFDrawingRefView: KFPostRefView, NSXMLParserDelegate{
     
     //let ref: KFReference;
     var webView:KFWebView!;
+    var imgview:UIImageView?;//tmp for ios7
     var svgwidth = CGFloat(100.0);
     var svgheight = CGFloat(100.0);
     var rotation = CGFloat(0.0);
@@ -43,18 +44,51 @@ class KFDrawingRefView: KFPostRefView, NSXMLParserDelegate{
         
         let baseURLStr = "http://" + KFService.getInstance().getHost();
         let baseURL = NSURL(string: baseURLStr);
-        webView.loadHTMLString(drawing.content, baseURL: baseURL);
         
+        var systemVersion = UIDevice.currentDevice().systemVersion;
+        if(systemVersion.hasPrefix("7.") && imageName != nil){
+            //no way to load UIWebview the site while comet working
+            let html = "<html><head></head><body><p>image not support in ios7</p><img src='\(imageName!)' width='\(svgwidth) height='\(svgheight)'/></body></html>";
+            webView.loadHTMLString(html, baseURL: baseURL);
+            webView.layer.borderColor = UIColor.grayColor().CGColor;
+            webView.layer.borderWidth = 1;
+            
+            KFAppUtils.executeInBackThread({
+                let fullURL = baseURLStr + self.imageName!;
+                let req = KFHttpRequest(urlString: fullURL, method: "GET");
+                let res = KFHttpConnection.connect(req);
+                if(res.getStatusCode() == 200){
+                    KFAppUtils.executeInGUIThread({
+                        let img = UIImage(data: res.bodyData!);
+                        self.imgview = UIImageView(image: img);
+                        self.imgview!.frame = CGRectMake(0, 0, self.svgwidth, self.svgheight);
+                        self.addSubview(self.imgview!);
+                        self.updateTransform();
+                        return;
+                    });
+                }
+                return;
+            });
+            return;
+        }
+        
+        webView.loadHTMLString(drawing.content, baseURL: baseURL);
         
         //println(model.width);
         //println(model.height);
         //println(model.rotation);
     }
     
+    //for ios 7
+    var imageName:String?;
+    
     func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!, attributes attributeDict: NSDictionary!){
         if(elementName == "svg"){
             svgwidth =  CGFloat((attributeDict["width"] as NSString).floatValue);
             svgheight =  CGFloat((attributeDict["height"] as NSString).floatValue);
+        }
+        if(elementName == "image"){
+            imageName = attributeDict["xlink:href"] as String?;
         }
     }
     
@@ -74,7 +108,7 @@ class KFDrawingRefView: KFPostRefView, NSXMLParserDelegate{
         let newWidth = scaleX * svgwidth;
         let newHeight = scaleY * svgheight;
         self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, newWidth,newHeight);
-        webView.transform = CGAffineTransformMakeScale(self.scaleX, self.scaleY);
+        webView.transform = CGAffineTransformMakeScale(self.scaleX, self.scaleY);//necessary
         
         //let rotatedWidth = cos(rotation)*newWidth + sin(rotation)*newHeight;
         //let rotatedHeight = cos(rotation)*newHeight + sin(rotation)*newWidth;
@@ -110,6 +144,13 @@ class KFDrawingRefView: KFPostRefView, NSXMLParserDelegate{
         let scaleT = CGAffineTransformMakeScale(self.scaleX, self.scaleY);
         let rotationT = CGAffineTransformMakeRotation(self.rotation);
         webView.transform = CGAffineTransformConcat(rotationT, scaleT);
+        if(self.imgview != nil){
+            let newWidth = scaleX * svgwidth;
+            let newHeight = scaleY * svgheight;
+            imgview!.transform = CGAffineTransformIdentity;
+            imgview!.frame = CGRectMake(0, 0, newWidth, newHeight);
+            imgview!.transform = rotationT;//CGAffineTransformConcat(scaleT, rotationT);
+        }
     }
     
     //    override func handleSingleTap(recognizer: UIGestureRecognizer){
